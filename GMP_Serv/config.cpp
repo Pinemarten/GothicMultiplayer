f062b5c6f1a9ecc8b0a20b2c797b279aa72f7f86
skejt23 <skejt23@gmail.com>
2022-08-20 01:39:13 +0200

    fix(server): include config.h typo

--- GMP_Serv/config.cpp
+++ GMP_Serv/config.cpp
@@ -23,7 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#include "Config.h"
+#include "config.h"
 #include "TomlWrapper.h"
 
 #include <spdlog/fmt/ostr.h>
