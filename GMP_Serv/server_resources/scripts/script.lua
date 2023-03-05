0bccd605bf0eb46840d5043d33c4dd0e763869ec
skejt23 <skejt23@gmail.com>
2023-03-05 12:39:04 +0100

    chore: update example lua script to use new events

--- GMP_Serv/server_resources/scripts/script.lua
+++ GMP_Serv/server_resources/scripts/script.lua
@@ -1,1 +1,9 @@
 print('Hello GMP')
+
+addEventHandler('OnClockUpdate', function(day, hour, minute)
+    print("Server clock has been updated. Day " .. day .. " Hour " .. hour .. " Minute " .. minute .. ".")
+end)
+
+addEventHandler('OnPlayerConnect', function(id)
+    print("Player " .. id .. " has connected.")
+end)
