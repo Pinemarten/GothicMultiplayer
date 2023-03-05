afd31b95aba3a7582f2a6abe7bcd2097941f7d61
skejt23 <skejt23@gmail.com>
2023-03-05 15:35:20 +0100

    feat: allow formatting in log functions in lua (python-like)

--- GMP_Serv/server_resources/scripts/script.lua
+++ GMP_Serv/server_resources/scripts/script.lua
@@ -1,5 +1,11 @@
 print('Hello GMP')
 
+-- Test logging functions
+LOG_WARN("Easy padding in numbers like {:08d}", 12);
+LOG_CRITICAL("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+LOG_INFO("Positional args are {1} {0}..", "too", "supported");
+LOG_INFO("{:>8} aligned, {:<8} aligned", "right", "left");
+
 addEventHandler('OnClockUpdate', function(day, hour, minute)
     print("Server clock has been updated. Day " .. day .. " Hour " .. hour .. " Minute " .. minute .. ".")
 end)
